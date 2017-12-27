56c9e910a5e9f49a05ff3eb0c853b5c4a47f75a3
douyacai911 <douyacai911@gmail.com>
2017-12-27 17:23:15 +0900

    写入文件

--- GetRank.py
+++ GetRank.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import re
+import os
 from urllib import request
 from urllib.parse import quote
 import string
@@ -7,7 +10,7 @@ from bs4 import BeautifulSoup
 '''
 Created on 2017/12/27
 
-@author: douyacai911
+@author: liscmb
 '''
 response = request.urlopen("https://movie.douban.com/chart")
 html = response.read().decode("utf-8")
@@ -16,15 +19,24 @@ soup = BeautifulSoup(html,"lxml")
 
 print("OK")
 
+fw = open("./filelist", "w",encoding='utf-8')
+
+
 for child in soup.body.find_all(href=re.compile("typerank\?type_name")):
     print(child.text)
+    fw.write(child.text+"\n")
     typeNo = re.findall(r"type=(.+?)&amp;",str(child))[0]
     response3 = request.urlopen('https://movie.douban.com/j/chart/top_list?type=' +typeNo+ '&interval_id=100%3A90&action=&start=0&limit=20')
     text = json.loads(response3.read().decode("utf-8"))
     print(text)
     print("Rank\tRating\tTitle\tUrl")
+    fw.write("Rank\tRating\tTitle\tUrl"+"\n")
     for mo in text:
-        print(str(mo.get('rank')) + "\t" + mo.get('rating')[0] + "\t" + mo.get('title') + "\t" + mo.get('url'))
+        info = str(mo.get('rank')) + "\t" + mo.get('rating')[0] + "\t" + mo.get('title') + "\t" + mo.get('url')+"\n"
+        print(info)
+        fw.write(info)
+
+fw.close()
 #     s = quote('https://movie.douban.com'+ str(child.get('href')), safe=string.printable)
 #     print(s)
 #     response2 = request.urlopen(s)
